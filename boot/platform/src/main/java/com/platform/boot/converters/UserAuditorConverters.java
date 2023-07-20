b8cd194c66eeec510447cf3139eb462e88032871
AlexBob <billbob.dong@gmail.com>
2023-07-20 09:21:44 +0800

    #001 update utils coding. format code text.

--- boot/platform/src/main/java/com/platform/boot/converters/UserAuditorConverters.java
+++ boot/platform/src/main/java/com/platform/boot/converters/UserAuditorConverters.java
@@ -1,12 +1,18 @@
 package com.platform.boot.converters;
 
 import com.platform.boot.security.UserAuditor;
+import com.platform.boot.security.user.User;
+import com.platform.boot.security.user.UsersRepository;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.convert.WritingConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import java.time.Duration;
 
 /**
  * This class contains converters for UserAuditor objects.
@@ -33,7 +39,10 @@ public class UserAuditorConverters {
 
     @Component
     @ReadingConverter
+    @RequiredArgsConstructor
     public static class UserAuditorReadConverter implements Converter<String, UserAuditor> {
+
+        private final UsersRepository usersRepository;
 
         /**
          * Converts a string to a UserAuditor object.
@@ -43,7 +52,13 @@ public class UserAuditorConverters {
          */
         @Override
         public UserAuditor convert(@NonNull String source) {
-            return UserAuditor.withUsername(source);
+            UserAuditor userAuditor = UserAuditor.withUsername(source);
+            User user = this.usersRepository.findByUsernameIgnoreCase(source)
+                    .timeout(Duration.ofSeconds(1)).share().block();
+            if (!ObjectUtils.isEmpty(user)) {
+                userAuditor.setName(user.getName());
+            }
+            return userAuditor;
         }
     }
 }
