16d2c2965a1cf465efe2bf451c07f7e0b0d3c51e
AlexBob <billbob.dong@gmail.com>
2023-10-27 17:55:05 +0800

    优化 records 类的编码方式

--- boot/platform/src/main/java/com/platform/boot/commons/query/ParamSql.java
+++ boot/platform/src/main/java/com/platform/boot/commons/query/ParamSql.java
@@ -2,19 +2,35 @@ package com.platform.boot.commons.query;
 
 import com.google.common.collect.Maps;
 
+import java.io.Serializable;
 import java.util.Map;
 import java.util.StringJoiner;
 
 /**
+ * ParamSql represents a SQL query with parameters.
+ * It includes a SQL string and a map of parameter values.
+ * The whereSql() method returns the WHERE clause of the SQL query.
  * @author <a href="https://github.com/vnobo">Alex bob</a>
  */
-public record ParamSql(StringJoiner sql, Map<String, Object> params) {
+public record ParamSql(StringJoiner sql, Map<String, Object> params) implements Serializable {
     public final static ParamSql EMPTY = ParamSql.of(new StringJoiner(" AND "), Maps.newHashMap());
 
+    /**
+     * Creates a new ParamSql instance with the given SQL string and parameters.
+     *
+     * @param sql    The SQL string.
+     * @param params The map of parameter values.
+     * @return A new ParamSql instance.
+     */
     public static ParamSql of(StringJoiner sql, Map<String, Object> params) {
         return new ParamSql(sql, params);
     }
 
+    /**
+     * Returns the WHERE clause of the SQL query.
+     *
+     * @return The WHERE clause.
+     */
     public String whereSql() {
         if (this.sql.length() > 0) {
             return " Where " + this.sql;
