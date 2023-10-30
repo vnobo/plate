f4db125e63dc6b94e1fba4215f8baff91525f1e0
AlexBob <billbob.dong@gmail.com>
2023-10-30 17:52:34 +0800

    filter logger format message.

--- boot/platform/src/main/java/com/platform/boot/commons/utils/BeanUtils.java
+++ boot/platform/src/main/java/com/platform/boot/commons/utils/BeanUtils.java
@@ -44,27 +44,12 @@ public class BeanUtils {
         }
     }
 
-    /**
-     * This is a static method that copies the properties of a given source object to a given target object.
-     *
-     * @param <T>    The type of the target object
-     * @param source The source object to be copied
-     * @param clazz  The class of the target object
-     * @return T The target object with the properties of the source object
-     */
     public static <T> T copyProperties(Object source, Class<T> clazz) {
         T target = org.springframework.beans.BeanUtils.instantiateClass(clazz);
         BeanUtils.copyProperties(source, target, false);
         return target;
     }
 
-    /**
-     * copy source to target object if source is not null.
-     * ignore source null properties.
-     *
-     * @param source source object
-     * @param target target object
-     */
     public static void copyProperties(Object source, Object target) {
         BeanUtils.copyProperties(source, target, true);
     }
@@ -79,39 +64,14 @@ public class BeanUtils {
         org.springframework.beans.BeanUtils.copyProperties(source, target, nullKeys);
     }
 
-    /**
-     * This method is used to convert a java bean to a map.
-     * Default no ignore Null Value.
-     *
-     * @param bean bean to be converted
-     * @param <T>  generic type
-     * @return result of conversion as a map
-     */
     public static <T> Map<String, Object> beanToMap(T bean) {
         return BeanUtils.beanToMap(bean, false);
     }
 
-    /**
-     * This method is used to convert a java bean to a map.
-     *
-     * @param bean            bean to be converted
-     * @param ignoreNullValue boolean flag to ignore null values
-     * @param <T>             generic type
-     * @return result of conversion as a map
-     */
     public static <T> Map<String, Object> beanToMap(T bean, final boolean ignoreNullValue) {
         return BeanUtils.beanToMap(bean, false, ignoreNullValue);
     }
 
-    /**
-     * This method is used to convert Java bean to Map
-     *
-     * @param bean              Java bean to be converted
-     * @param ignoreNullValue   whether to ignore null value in bean or not
-     * @param isToUnderlineCase determines if the converted Map should have underlined keys
-     * @param <T>               type of the bean
-     * @return a Map containing the values from the bean
-     */
     public static <T> Map<String, Object> beanToMap(T bean, final boolean isToUnderlineCase, final boolean ignoreNullValue) {
         if (ObjectUtils.isEmpty(bean)) {
             return null;
