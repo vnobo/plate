6d143f607898f53e41f3bcb26c4171db4d637978
AlexBob <billbob.dong@gmail.com>
2023-11-22 17:35:58 +0800

    优化UI angular project

---
+++ ui/projects/web/src/app/pages/pages.routes.ts
@@ -0,0 +1,35 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {IndexComponent} from "./home/index/index.component";
+import {HomeComponent} from "./home/home.component";
+import {authGuard} from "../core/auth.service";
+
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [authGuard],
+    component: HomeComponent,
+    children: [
+      {
+        path: 'system',
+        canActivateChild: [authGuard],
+        loadChildren: () => import('./system/system.module').then(m => m.SystemModule),
+        title: '系统管理',
+      },
+      {
+        path: '',
+        canActivateChild: [authGuard],
+        component: IndexComponent,
+        title: '首页'
+      }
+    ]
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule {
+}
