3754b5086506ec186461829da2f0832f8f159619
AlexBob <billbob.dong@gmail.com>
2023-11-28 01:15:39 +0800

    ng add @angular-eslint/schematics

--- ui/projects/web/src/app/pages/home/index/index.component.ts
+++ ui/projects/web/src/app/pages/home/index/index.component.ts
@@ -1,12 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent implements OnInit {
-
-  ngOnInit() {
-  }
+export class IndexComponent {
 }
