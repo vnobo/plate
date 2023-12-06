55aa7a63b095a4f5bfa5b8f4944c4937b7d79b85
AlexBob <billbob.dong@gmail.com>
2023-12-06 16:27:06 +0800

    Bean object size. fix bug search error.

---
+++ ui/projects/web/src/app/pages/system/users/users.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  template: `<p>users works!</p>`,
+  styleUrl: './users.component.css'
+})
+export class UsersComponent {
+}
