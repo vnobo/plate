42b9639d3c9c18207c4af153c59477ba244c7088
AlexBob <billbob.dong@gmail.com>
2024-01-08 18:11:54 +0800

    update spring boot 3.2.1

--- boot/platform/src/main/java/com/platform/boot/commons/query/QueryJson.java
+++ boot/platform/src/main/java/com/platform/boot/commons/query/QueryJson.java
@@ -2,9 +2,11 @@ package com.platform.boot.commons.query;
 
 import com.google.common.base.CaseFormat;
 import com.google.common.collect.Maps;
+import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringJoiner;
@@ -18,6 +20,8 @@ public class QueryJson {
     private final static Map<String, String> KEYWORDS = Maps.newHashMap();
 
     static {
+        KEYWORDS.put("EQ", "=");
+        KEYWORDS.put("Equal", "=");
         KEYWORDS.put("After", ">");
         KEYWORDS.put("GreaterThanEqual", ">=");
         KEYWORDS.put("GTE", ">=");
@@ -64,35 +68,36 @@ public class QueryJson {
         Map<String, Object> bindParams = Maps.newHashMap();
         StringJoiner whereSql = new StringJoiner(" and ");
         for (Map.Entry<String, Object> entry : params.entrySet()) {
-            String[] keys = StringUtils.delimitedListToStringArray(entry.getKey(), "Query");
-            String column = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, keys[0]);
-            Map.Entry<String, String> exps = jsonPathKeyAndParamName(column, keys[1]);
+            String[] keys = StringUtils.delimitedListToStringArray(entry.getKey(), ".");
+            Map.Entry<String, String> exps = jsonPathKeyAndParamName(keys);
             whereSql.add(exps.getValue());
             bindParams.put(exps.getKey(), entry.getValue());
         }
         return ParamSql.of(whereSql, bindParams);
     }
 
-    private static Map.Entry<String, String> jsonPathKeyAndParamName(String column, String key) {
-        Map.Entry<String, String> exps = exitsKeyWords(key);
-        String source = ObjectUtils.isEmpty(exps) ? key : key.substring(0, key.length() - exps.getKey().length());
-        String[] paths = StringUtils.delimitedListToStringArray(source, ".");
-        StringBuilder jsonPath = new StringBuilder(column);
-        int index = 0;
-        for (String path : paths) {
+    private static Map.Entry<String, String> jsonPathKeyAndParamName(String[] keys) {
+        String lastKey = keys[keys.length - 1];
+        String colum = keys[0];
+
+        Map.Entry<String, String> exps = exitsKeyWords(lastKey);
+
+        StringBuilder jsonPath = new StringBuilder(colum);
+
+        String[] joinKeys = Arrays.copyOfRange(keys, 1, keys.length - 1);
+        ;
+        for (String path : joinKeys) {
             var capath = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, path);
-            if (index < paths.length - 1) {
-                jsonPath.append("->'").append(capath).append("'");
-            } else {
-                jsonPath.append("->>'").append(capath).append("' ");
-            }
-            index++;
+            jsonPath.append("->'").append(capath).append("'");
         }
-        String paramName = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, source.replace(".", "_"));
+
+        String key = lastKey.substring(0, lastKey.length() - exps.getKey().length());
+        jsonPath.append("->>'").append(key).append("' ");
+        String paramName = StringUtils.arrayToDelimitedString(keys, "_");
         if (!ObjectUtils.isEmpty(exps)) {
             jsonPath.append(exps.getValue()).append(" :").append(paramName);
         } else {
-            jsonPath.append("= ").append(":").append(paramName);
+            jsonPath.append("=").append(" :").append(paramName);
         }
         return Map.entry(paramName, jsonPath.toString());
     }
@@ -101,9 +106,7 @@ public class QueryJson {
         Set<Map.Entry<String, String>> entries = KEYWORDS.entrySet().stream()
                 .filter(entry -> StringUtils.endsWithIgnoreCase(inputStr, entry.getKey()))
                 .collect(Collectors.toSet());
-        if (entries.isEmpty()) {
-            return null;
-        }
+        Assert.notNull(entries, "Not support key words: " + inputStr);
         return entries.stream().max((entry1, entry2) -> {
             int entry1Length = entry1.getKey().length();
             int entry2Length = entry2.getKey().length();
