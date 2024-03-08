8cbf581544d9f04a43bd96872ecd9dff3e665efb
AlexBob <billbob.dong@gmail.com>
2024-03-08 18:03:24 +0800

    add module authorization server.

--- boot/authorization/src/main/java/com/plate/authorization/config/AuthorizationConfig.java
+++ boot/authorization/src/main/java/com/plate/authorization/config/AuthorizationConfig.java
@@ -6,5 +6,5 @@ import org.springframework.context.annotation.Configuration;
  * @author Alex bob(<a href="https://github.com/vnobo">Alex Bob</a>)
  */
 @Configuration
-public class AuthorizationServerConfig {
+public class AuthorizationConfig {
 }
