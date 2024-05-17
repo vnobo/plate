b48e7a6a605eab60f2d7f88b9747f3823d9972e6
AlexBob <5199840@qq.com>
2024-05-17 10:46:03 +0800

    ♻️ refactor: 移除未使用的方法

--- boot/platform/src/main/java/com/platform/boot/security/core/user/UserResponse.java
+++ boot/platform/src/main/java/com/platform/boot/security/core/user/UserResponse.java
@@ -1,13 +1,9 @@
 package com.platform.boot.security.core.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.platform.boot.security.core.UserAuditor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-
-import java.time.LocalDateTime;
 
 /**
  * @author <a href="https://github.com/vnobo">Alex bob</a>
@@ -19,85 +15,8 @@ public class UserResponse extends User {
 
     @JsonIgnore
     @Override
-    public String getPhone() {
-        return super.getPhone();
-    }
-
-    @JsonIgnore
-    @Override
-    public String getEmail() {
-        return super.getEmail();
-    }
-
-    @JsonIgnore
-    @Override
     public String getPassword() {
         return super.getPassword();
     }
 
-    @JsonIgnore
-    @Override
-    public String getUsername() {
-        return super.getUsername();
-    }
-
-    @JsonIgnore
-    @Override
-    public Boolean getDisabled() {
-        return super.getDisabled();
-    }
-
-    @JsonIgnore
-    @Override
-    public Boolean getAccountExpired() {
-        return super.getAccountExpired();
-    }
-
-    @JsonIgnore
-    @Override
-    public Boolean getAccountLocked() {
-        return super.getAccountLocked();
-    }
-
-    @JsonIgnore
-    @Override
-    public Boolean getCredentialsExpired() {
-        return super.getCredentialsExpired();
-    }
-
-    @JsonIgnore
-    @Override
-    public JsonNode getExtend() {
-        return super.getExtend();
-    }
-
-    @JsonIgnore
-    @Override
-    public LocalDateTime getLoginTime() {
-        return super.getLoginTime();
-    }
-
-    @JsonIgnore
-    @Override
-    public UserAuditor getCreator() {
-        return super.getCreator();
-    }
-
-    @JsonIgnore
-    @Override
-    public UserAuditor getUpdater() {
-        return super.getUpdater();
-    }
-
-    @JsonIgnore
-    @Override
-    public LocalDateTime getUpdatedTime() {
-        return super.getUpdatedTime();
-    }
-
-    @JsonIgnore
-    @Override
-    public LocalDateTime getCreatedTime() {
-        return super.getCreatedTime();
-    }
 }
