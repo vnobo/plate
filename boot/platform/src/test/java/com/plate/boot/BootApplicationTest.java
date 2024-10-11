a58a34a486a1355a01d1a07d832d17ae53c005e3
AlexBob <billbob.dong@gmail.com>
2024-10-11 09:47:34 +0800

    - 删除了初始的 index.html 文件，该文件包含静态内容和Bootstrap样式 - 移除了 QueryHelperTest 类，该类包含对 QueryHelper 类的单元测试 - 添加了 WebConfigurationTest 类，该类为测试配置类，设置了自定义参数解析器 - 删除了 UsersControllerTest 类，该类包含对 UsersController 的集成测试 - 更新了 BootApplicationTest 类，添加了 WebConfigurationTest 的导入，并调整了测试方法的名称 - 添加了 application-test.yml 文件，配置了日志、数据库连接、Redis和OAuth2客户端注册信息

--- boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java
+++ boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java
@@ -1,22 +1,29 @@
 package com.plate.boot;
 
+import com.plate.boot.config.WebConfigurationTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import java.security.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author <a href="https://github.com/vnobo">Alex Bob</a>
  */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "spring.profiles.active=test")
+@Import({WebConfigurationTest.class})
 class BootApplicationTest {
 
     @Test
-    void contextLoads() throws NoSuchAlgorithmException {
+    void contextLoadsTest() throws NoSuchAlgorithmException {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(2048);
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
         PrivateKey privateKey = keyPair.getPrivate();
         PublicKey publicKey = keyPair.getPublic();
-        System.out.println(privateKey);
-        System.out.println(publicKey);
+        assertThat(privateKey).isNotNull();
+        assertThat(publicKey).isNotNull();
     }
 }
