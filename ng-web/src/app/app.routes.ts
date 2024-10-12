c230a1da0056692a7fc0d8b71943760d3ec3f1f5
AlexBob <billbob.dong@gmail.com>
2024-10-12 15:07:54 +0800

    Remove duplicate route definition for 'welcome'

--- ng-web/src/app/app.routes.ts
+++ ng-web/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
   { path: 'home', loadChildren: () => import('./pages/home/home.routes').then(m => m.HOME_ROUTES) },
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
 ];
