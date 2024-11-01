16618d29e9b0005349dbf9c0e0656a489f75a773
AlexBob <billbob.dong@gmail.com>
2024-11-01 17:21:31 +0800

    Add .prettierignore, .prettierrc.json, and .vscode/extensions.json, update angular.json and package-lock.json, and introduce @angular/localize

--- ng-ui/src/app/app.component.ts
+++ ng-ui/src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-ui';
+  timeNow = new Date();
 }
--- ng-ui/src/app/app.config.ts
+++ ng-ui/src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    { provide: LOCALE_ID, useValue: 'zh_CN' },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+  ],
 };
