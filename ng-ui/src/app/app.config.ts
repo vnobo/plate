d26f979241e820a878036f0653634422831abb44
AlexBob <billbob.dong@gmail.com>
2024-11-04 10:57:13 +0800

    Update angular.json and package-lock.json: remove zone.js, add @angular/material and bootstrap dependencies, and update assets and styles

--- ng-ui/src/app/app.config.ts
+++ ng-ui/src/app/app.config.ts
@@ -1,28 +1,24 @@
-import { ApplicationConfig, importProvidersFrom, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  LOCALE_ID,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import { icons } from './icons-provider';
 import { provideNzIcons } from 'ng-zorro-antd/icon';
 import { provideNzI18n, zh_CN } from 'ng-zorro-antd/i18n';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-  withInterceptorsFromDi,
-  withXsrfConfiguration,
-} from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi, withXsrfConfiguration } from '@angular/common/http';
 import { indexInterceptor } from './core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    { provide: LOCALE_ID, useValue: 'zh_CN' },
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
-    provideClientHydration(),
     provideNzIcons(icons),
     provideNzI18n(zh_CN),
     importProvidersFrom(FormsModule),
@@ -36,5 +32,9 @@ export const appConfig: ApplicationConfig = {
         headerName: 'X-XSRF-TOKEN',
       }),
     ),
+    { provide: LOCALE_ID, useValue: 'zh_CN' },
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(withEventReplay()),
+    provideExperimentalZonelessChangeDetection(), provideAnimationsAsync(),
   ],
 };
