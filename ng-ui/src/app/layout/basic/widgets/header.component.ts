4dfda4c590d5748e5da96c1ad5cb791a4f87f0be
AlexBob <billbob.dong@gmail.com>
2024-11-18 17:56:28 +0800

    ✨ feat(`header.component.ts`, `users.component.html`, `styles/index.scss`, `styles/theme.less`): Refactor header component, users component template, and update theme styles for improved UI and maintainability.

--- ng-ui/src/app/layout/basic/widgets/header.component.ts
+++ ng-ui/src/app/layout/basic/widgets/header.component.ts
@@ -9,32 +9,29 @@ import { SharedModule } from '@app/shared/shared.module';
   selector: 'page-header,[pageHeader]',
   standalone: true,
   imports: [NzPageHeaderModule, NzTagModule, NzSpaceModule, SharedModule],
-  template: `
-    <div class="sticky-top">
-      <nz-page-header [nzGhost]="false" nzBackIcon>
-        <nz-breadcrumb [nzAutoGenerate]="true" [nzRouteLabel]="'title'" nz-page-header-breadcrumb></nz-breadcrumb>
-        <nz-page-header-title>
-          <ng-content select="[pageHeaderTitle]">headerTitle</ng-content>
-        </nz-page-header-title>
-        <nz-page-header-subtitle>
-          <ng-content select="[pageHeaderSubtitle]">headerSubtitle</ng-content>
-        </nz-page-header-subtitle>
-        <nz-page-header-tags>
-          <ng-content select="[pageHeaderTags]">headerTags</ng-content>
-        </nz-page-header-tags>
-        <nz-page-header-extra>
-          <ng-content select="[pageHeaderExtra]">headerExtra</ng-content>
-        </nz-page-header-extra>
-        <nz-page-header-content>
-          <ng-content select="[pageHeaderContent]">headerContent</ng-content>
-        </nz-page-header-content>
-      </nz-page-header>
-    </div>`,
+  template: ` <div class="sticky-top">
+    <nz-page-header [nzGhost]="false" nzBackIcon>
+      <nz-breadcrumb [nzAutoGenerate]="true" [nzRouteLabel]="'title'" nz-page-header-breadcrumb></nz-breadcrumb>
+      <nz-page-header-title>
+        <ng-content select="[pageHeaderTitle]"></ng-content>
+      </nz-page-header-title>
+      <nz-page-header-subtitle>
+        <ng-content select="[pageHeaderSubtitle]"></ng-content>
+      </nz-page-header-subtitle>
+      <nz-page-header-tags>
+        <ng-content select="[pageHeaderTags]"></ng-content>
+      </nz-page-header-tags>
+      <nz-page-header-extra>
+        <ng-content select="[pageHeaderExtra]"></ng-content>
+      </nz-page-header-extra>
+      <nz-page-header-content>
+        <ng-content select="[pageHeaderContent]"></ng-content>
+      </nz-page-header-content>
+    </nz-page-header>
+  </div>`,
 })
 export class PageHeaderComponent implements OnInit {
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
