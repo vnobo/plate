91db5c0e827f6e61859540925ae43276296330e2
Alex.Bob <billbob.dong@gmail.com>
2024-12-12 14:49:42 +0800

    ➖ feat(`boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java`): Remove unnecessary imports and annotations.

--- boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java
+++ boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java
@@ -1,14 +1,11 @@
 package com.plate.boot;
 
-import com.plate.boot.config.InfrastructureConfiguration;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author <a href="https://github.com/vnobo">Alex Bob</a>
  */
-@SpringBootTest(classes = InfrastructureConfiguration.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(classes = InfrastructureConfiguration.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BootApplicationTest {
 
     @Test
