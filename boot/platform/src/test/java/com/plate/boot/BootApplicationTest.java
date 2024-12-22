5c970b5ec8f5dc757f722760dd96e8500797d702
AlexBob <billbob.dong@gmail.com>
2024-12-22 13:00:34 +0800

    1. ✏️ fix(`boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java`): Remove unnecessary import and annotation. 2. ⬆️ refactor(`gradle/wrapper/gradle-wrapper.properties`): Upgrade Gradle version to 8.12.

--- boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java
+++ boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java
@@ -1,12 +1,10 @@
 package com.plate.boot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author <a href="https://github.com/vnobo">Alex Bob</a>
  */
-@SpringBootTest
 class BootApplicationTest {
 
     @Test
