4181eb74c86e4b4577579b46590d5a2fc5c61962
AlexBob <billbob.dong@gmail.com>
2024-12-27 15:07:27 +0800

    ✨ feat(`GroupMembersService`, `GroupMemberReq`): Implement dynamic SQL queries for group members search and pagination.

--- boot/platform/src/main/java/com/plate/boot/security/core/group/member/GroupMembersService.java
+++ boot/platform/src/main/java/com/plate/boot/security/core/group/member/GroupMembersService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.relational.core.query.Criteria;
 import org.springframework.data.relational.core.query.Query;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -18,31 +19,32 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 public class GroupMembersService extends AbstractDatabase {
-    private final static String QUERY_SQL = """
-            select a.*, b.name as group_name, b.extend as group_extend,c.name as login_name,c.username
-            from se_group_members a
-            inner join se_groups b on a.group_code = b.code
-            inner join se_users c on c.code = a.user_code
-            """;
-    private final static String COUNT_SQL = """
-            select count(*) from se_group_members a
-            inner join se_groups b on a.group_code = b.code
-            inner join se_users c on c.code = a.user_code
-            """;
 
     private final GroupMembersRepository memberRepository;
 
     public Flux<GroupMemberResp> search(GroupMemberReq request, Pageable pageable) {
-        QueryFragment queryFragment = request.toParamSql();
+        QueryFragment fragment = request.toParamSql();
+        QueryFragment queryFragment = QueryFragment.withColumns("a.*", "b.name as group_name", "b.extend as group_extend",
+                        "c.name as login_name", "c.username")
+                .from("se_group_members a",
+                        "inner join se_groups b on a.group_code = b.code",
+                        "inner join se_users c on c.code = a.user_code")
+                .where(fragment.getWhere().toString());
+        queryFragment.putAll(fragment);
+        Query.query(Criteria.where("name").in("a"));
         return super.queryWithCache(BeanUtils.cacheKey(request, pageable), queryFragment.querySql(),
                 queryFragment, GroupMemberResp.class);
     }
 
     public Mono<Page<GroupMemberResp>> page(GroupMemberReq request, Pageable pageable) {
         var searchMono = this.search(request, pageable).collectList();
-        QueryFragment QueryFragment = request.toParamSql();
-        String query = COUNT_SQL + QueryFragment.whereSql();
-        var countMono = this.countWithCache(BeanUtils.cacheKey(request), query, QueryFragment);
+        QueryFragment fragment = request.toParamSql();
+        QueryFragment queryFragment = QueryFragment.withColumns("*")
+                .from("se_group_members a",
+                        "inner join se_groups b on a.group_code = b.code",
+                        "inner join se_users c on c.code = a.user_code")
+                .where(fragment.getWhere().toString());
+        var countMono = this.countWithCache(BeanUtils.cacheKey(request), queryFragment.countSql(), queryFragment);
         return searchMono.zipWith(countMono)
                 .map(tuple2 -> new PageImpl<>(tuple2.getT1(), pageable, tuple2.getT2()));
     }
