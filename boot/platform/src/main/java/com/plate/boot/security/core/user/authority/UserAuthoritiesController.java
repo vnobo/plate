0b99e30531dff56859fa9930b095937aef981509
AlexBob <billbob.dong@gmail.com>
2025-01-06 16:54:19 +0800

    ✨ feat(`UserAuthoritiesController`): Update request types to `UserAuthorityReq`.

--- boot/platform/src/main/java/com/plate/boot/security/core/user/authority/UserAuthoritiesController.java
+++ boot/platform/src/main/java/com/plate/boot/security/core/user/authority/UserAuthoritiesController.java
@@ -18,17 +18,17 @@ public class UserAuthoritiesController {
     private final UserAuthoritiesService authoritiesService;
 
     @GetMapping("search")
-    public Flux<UserAuthority> search(UserAuthorityRequest request) {
+    public Flux<UserAuthority> search(UserAuthorityReq request) {
         return this.authoritiesService.search(request);
     }
 
     @PostMapping("save")
-    public Mono<UserAuthority> save(@Valid @RequestBody UserAuthorityRequest request) {
+    public Mono<UserAuthority> save(@Valid @RequestBody UserAuthorityReq request) {
         return this.authoritiesService.operate(request);
     }
 
     @DeleteMapping("delete")
-    public Mono<Void> delete(@Valid @RequestBody UserAuthorityRequest request) {
+    public Mono<Void> delete(@Valid @RequestBody UserAuthorityReq request) {
         Assert.notNull(request.getId(), "删除用户时，[ID]不能为空");
         return this.authoritiesService.delete(request);
     }
