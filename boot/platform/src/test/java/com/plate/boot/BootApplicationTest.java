55d7cf4b3165742edb0a86fb7160d40a140981a5
AlexBob <billbob.dong@gmail.com>
2025-01-17 11:30:30 +0800

    ✨ feat(test): Add Spring Boot test configuration and update `.gitattributes`
    This commit adds a new test configuration for the Spring Boot application and updates the `.gitattributes` file to ensure proper line endings and handling of binary files.

--- boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java
+++ boot/platform/src/test/java/com/plate/boot/BootApplicationTest.java
@@ -1,28 +1,15 @@
 package com.plate.boot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.MalformedURLException;
-import java.net.URI;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author <a href="https://github.com/vnobo">Alex Bob</a>
  */
+@SpringBootTest
 class BootApplicationTest {
 
     @Test
-    void contextLoads() throws MalformedURLException {
-        URI uri = UriComponentsBuilder.fromUriString("https://tcszxsj.oss-cn-chengdu.aliyuncs.com/" +
-                "template/social/610222000000000001/cppcc/socialPaper.docx").build().toUri();
-        Resource resource = new UrlResource(uri);
-        String filename = resource.getFilename();
-        System.out.println(filename);
-        URI parent = uri.resolve("..");
-        System.out.println(parent);
-        URI fileUri = UriComponentsBuilder.fromUri(parent).path(filename).build().toUri();
-        System.out.println(fileUri);
+    void contextLoads() {
     }
 }
