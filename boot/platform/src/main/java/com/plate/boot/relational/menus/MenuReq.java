53bc0a67940af8231611d923f824cf4e90a38d1a
AlexBob <billbob.dong@gmail.com>
2025-01-22 02:24:40 +0800

    ✨ feat(`MenuReq.java`): Expand MenuReq with additional properties, methods, and documentation.

--- boot/platform/src/main/java/com/plate/boot/relational/menus/MenuReq.java
+++ boot/platform/src/main/java/com/plate/boot/relational/menus/MenuReq.java
@@ -16,20 +16,43 @@ import java.util.Optional;
 import java.util.Set;
 
 /**
- * @author <a href="https://github.com/vnobo">Alex Bob</a>
+ * MenuReq class extends Menu and provides additional properties and methods
+ * for handling menu requests.
+ *
+ * <p>This class includes properties for rules, icons, and nested menus, and
+ * provides methods to convert to a Menu object and to create query criteria.
+ * </p>
+ *
+ * <p>Author: <a href="https://github.com/vnobo">Alex Bob</a></p>
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class MenuReq extends Menu {
 
+    /**
+     * Set of rules associated with the menu request.
+     */
     private Set<String> rules;
 
+    /**
+     * Icons associated with the menu request.
+     */
     private String icons;
 
+    /**
+     * Set of nested MenuReq objects.
+     */
     @Valid
     private Set<MenuReq> menus;
 
+    /**
+     * Creates a new MenuReq object with the specified tenant code and authority.
+     *
+     * @param tenantCode the tenant code
+     * @param authority  the authority
+     * @return a new MenuReq object
+     */
     public static MenuReq of(String tenantCode, String authority) {
         MenuReq menuReq = new MenuReq();
         menuReq.setTenantCode(tenantCode);
@@ -37,6 +60,11 @@ public class MenuReq extends Menu {
         return menuReq;
     }
 
+    /**
+     * Converts this MenuReq object to a Menu object.
+     *
+     * @return a Menu object
+     */
     public Menu toMenu() {
         Menu menu = BeanUtils.copyProperties(this, Menu.class);
         ObjectNode objectNode = Optional.ofNullable(this.getExtend())
@@ -49,6 +77,11 @@ public class MenuReq extends Menu {
         return menu;
     }
 
+    /**
+     * Creates a Criteria object based on the properties of this MenuReq object.
+     *
+     * @return a Criteria object
+     */
     public Criteria toCriteria() {
         Criteria criteria = criteria(Set.of("permissions", "tenantCode", "icons", "menus", "rules"));
 
