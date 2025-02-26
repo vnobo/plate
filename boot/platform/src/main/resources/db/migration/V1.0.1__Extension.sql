4e986c0ffee26f16b411f3807e66b3ca5e90d262
AlexBob <billbob.dong@gmail.com>
2025-02-26 15:02:47 +0800

    ✨ feat(`application.yml`): Add Flyway configuration for baseline migration. ⬇️ refactor(`build.gradle`): Downgrade Flyway dependencies to runtimeOnly. :truck: refactor(`db/migration`): Rename `V1.0.1__extension.sql` to `V1.0.1__Extension.sql`. :card_file_box: feat(`db/migration`): Add `V1.0.0__Baseline.sql` for Flyway baseline version. :art: style(`db/migration`): Improve SQL formatting and comments in `V1.0.1__Extension.sql`.

--- boot/platform/src/main/resources/db/migration/V1.0.1__Extension.sql
+++ boot/platform/src/main/resources/db/migration/V1.0.1__Extension.sql
@@ -7,22 +7,28 @@ create text search configuration chinese (parser = zhparser);
 
 -- add token mapping
 alter text search configuration chinese add mapping for n,v,a,i,e,l,x with simple;
+-- set default text search configuration is chinese
 set default_text_search_config = 'chinese';
+-- test default text search configuration
 show default_text_search_config;
+
+-- truncate custom word table
 truncate table zhparser.zhprs_custom_word;
 --add custom word
-insert into zhparser.zhprs_custom_word
-values ('支付宝');
-insert into zhparser.zhprs_custom_word
-values ('资金压力');
+insert into zhparser.zhprs_custom_word values ('支付宝'), ('资金压力');
+--sync custom word
 select sync_zhprs_custom_word();
-alter extension zhparser update;
+
+--test zhparser extension is ok
+
 select ts_token_type('zhparser');
 select ts_debug('chinese', '支付宝使用很方便');
 select ts_debug('chinese', '保障房资金压力');
+
 -- test custom word
 select ts_parse('zhparser', '支付宝使用很方便');
 select ts_parse('zhparser', '保障房资金压力');
+
 -- test to_tsvector
 select to_tsvector('chinese', '支付宝使用很方便');
 select to_tsquery('chinese', '支付宝使用很方便');
