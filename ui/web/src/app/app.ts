833dd83b2f177f3b774944a7d25b44fab0c8bc37
AlexBob <billbob.dong@gmail.com>
2025-06-05 17:23:35 +0800

    ✨ feat(`ui/web`): Add Angular Material integration with progress bar, auth guards, and core services, update theme and environment configs

--- ui/web/src/app/app.ts
+++ ui/web/src/app/app.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProgressBar } from './core/services/progress-bar';
+import { CommonModule } from '@angular/common';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [RouterOutlet, CommonModule, MatProgressBarModule],
+  template: ` @if (progressBar.isShow$ | async) {
+    <div class="fixed-top">
+      <mat-progress-bar mode="query"></mat-progress-bar>
+    </div>
+    }
+    <router-outlet />`,
   styles: [
     `
       :host {
@@ -15,5 +23,8 @@ import { RouterOutlet } from '@angular/router';
   ],
 })
 export class App {
-  protected title = 'web';
+  progressBar = inject(ProgressBar);
+  constructor(el: ElementRef, renderer: Renderer2) {
+    this.progressBar.show();
+  }
 }
