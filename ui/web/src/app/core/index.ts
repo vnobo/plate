478faa4b63bdc7f39cb9d4250080264a7894eadd
AlexBob <billbob.dong@gmail.com>
2025-06-15 14:38:47 +0800

    "✨ feat(`core/index.ts`, `login.ts`): Refactor TokenService export name and consolidate imports in login component"

--- ui/web/src/app/core/index.ts
+++ ui/web/src/app/core/index.ts
@@ -1,5 +1,5 @@
 export { indexInterceptor } from './net/http.Interceptor';
 export { PageTitleStrategy } from './services/page-title-strategy';
-export { TokenService as AuthService } from './services/token.service';
+export { TokenService } from './services/token.service';
 export { SessionStorageService } from './storage/session-storage';
 export { BrowserStorage } from './storage/browser-storage';
