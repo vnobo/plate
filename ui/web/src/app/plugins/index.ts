287594e666d11168f6bd92ffc5e68486227e3040
AlexBob <billbob.dong@gmail.com>
2025-06-16 15:29:50 +0800

    ⚡️ feat(`ui/web/src/app/plugins/index.ts`): Remove `AlertService` from plugin exports for reduced complexity.

--- ui/web/src/app/plugins/index.ts
+++ ui/web/src/app/plugins/index.ts
@@ -1,2 +1,2 @@
-export { Alerts, AlertService, type AlertType } from '../plugins/alerts/alerts';
+export { Alerts, type AlertType } from '../plugins/alerts/alerts';
 export { Toasts, MessageService, type ToastType } from '../plugins/toasts/toasts';
