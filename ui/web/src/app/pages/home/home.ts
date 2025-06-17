4c42f44a5585bca06cd2b1adeb6bf3d7592509f3
AlexBob <billbob.dong@gmail.com>
2025-06-17 16:00:29 +0800

    ✨ feat(`dashboard.ts`): Add welcome page routes ✨ feat(`home.ts`): Implement home routes with auth guard and role-based navigation

---
+++ ui/web/src/app/pages/home/home.ts
@@ -0,0 +1,40 @@
+import {Routes} from '@angular/router';
+import {MenusComponent} from './menus/menus.component';
+import {UsersComponent} from './users/users.component';
+import {GroupsComponent} from './groups/groups.component';
+import {authGuard} from '@app/core/pages.guard';
+
+export const HOME_ROUTES: Routes = [
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'roles',
+        data: {
+          title: '角色管理',
+        },
+        component: GroupsComponent,
+      },
+      {
+        path: 'users',
+        data: {
+          title: '用户管理',
+        },
+        component: UsersComponent,
+      },
+      {
+        path: 'menus',
+        data: {
+          title: '菜单管理',
+        },
+        component: MenusComponent,
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'users',
+      },
+    ],
+  },
+];
