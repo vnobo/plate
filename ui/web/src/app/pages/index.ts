69f1549e8feb7bbaa02ec80f8f003cae1b57923b
AlexBob <billbob.dong@gmail.com>
2025-06-17 15:59:16 +0800

    ✨ feat(`ui/web/src/app/pages/index.ts`): Add new route exports for Welcome and Home pages.
    ✏️ style(`ui/web/src/app/pages/passport/login/login.html`): Update login button styles and disable state logic.

--- ui/web/src/app/pages/index.ts
+++ ui/web/src/app/pages/index.ts
@@ -1,2 +1,4 @@
 export { PASSPORT_ROUTES } from './passport/passport';
 export { EXCEPTION_ROUTES } from './error/error';
+export { WELCOME_ROUTES } from './dashboard/dashboard';
+export { HOME_ROUTES } from './home/home';
