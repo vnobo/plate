aa160317d03959f94e2ad65ae1d2e45af97ee5a8
AlexBob <billbob.dong@gmail.com>
2025-06-18 17:14:31 +0800

    ✨ feat(`app.routes.ts`): Add 'home' route to application routes.

--- ui/web/src/app/app.routes.ts
+++ ui/web/src/app/app.routes.ts
@@ -1,10 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./pages/index').then(m => m.HOME_ROUTES),
+  },
+  {
     path: 'passport',
     loadChildren: () => import('./pages/index').then(m => m.PASSPORT_ROUTES),
-    data: { title: '管理平台' },
   },
   { path: 'exception', loadChildren: () => import('./pages/index').then(m => m.EXCEPTION_ROUTES) },
   { path: '', pathMatch: 'full', redirectTo: '/passport' },
