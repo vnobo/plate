c2a71bd1811b230535f226a095c384f866646c85
AlexBob <billbob.dong@gmail.com>
2025-08-21 15:48:20 +0800

    ♻️ refactor(query): migrate from sql string fragments to spring data criteria api
    * replace sql operation mapping with criteria function mapping
    * change queryjson method to return condition instead of queryfragment
    * update buildjsoncondition to use criteria api for dynamic query building
    * remove manual sql fragment construction in favor of spring data criteria
    * maintain same query functionality with improved type safety and maintainability

--- boot/platform/src/main/java/com/plate/boot/commons/query/QueryJsonHelper.java
+++ boot/platform/src/main/java/com/plate/boot/commons/query/QueryJsonHelper.java
@@ -5,11 +5,13 @@ import com.google.common.collect.Maps;
 import com.plate.boot.commons.exception.QueryException;
 import com.plate.boot.commons.exception.RestServerException;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.relational.core.query.Criteria;
 import org.springframework.util.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 /**
@@ -68,41 +70,61 @@ import java.util.stream.Collectors;
  */
 public final class QueryJsonHelper {
 
-    private final static Map<String, String> SQL_OPERATION_MAPPING = Maps.newHashMap();
+    private final static Map<String, BiFunction<String, Object, Criteria>> OPERATION_MAPPER = Maps.newHashMap();
 
     static {
-        SQL_OPERATION_MAPPING.put("EQ", "=");
-        SQL_OPERATION_MAPPING.put("Equal", "=");
-        SQL_OPERATION_MAPPING.put("After", ">");
-        SQL_OPERATION_MAPPING.put("GreaterThanEqual", ">=");
-        SQL_OPERATION_MAPPING.put("GTE", ">=");
-        SQL_OPERATION_MAPPING.put("GreaterThan", ">");
-        SQL_OPERATION_MAPPING.put("GT", ">");
-        SQL_OPERATION_MAPPING.put("Before", "<");
-        SQL_OPERATION_MAPPING.put("LessThanEqual", "<=");
-        SQL_OPERATION_MAPPING.put("LTE", "<=");
-        SQL_OPERATION_MAPPING.put("LessThan", "<");
-        SQL_OPERATION_MAPPING.put("LT", "<");
-        SQL_OPERATION_MAPPING.put("Between", "between");
-        SQL_OPERATION_MAPPING.put("NotBetween", "not between");
-        SQL_OPERATION_MAPPING.put("NotIn", "not in");
-        SQL_OPERATION_MAPPING.put("In", "in");
-        SQL_OPERATION_MAPPING.put("IsNotNull", "is not null");
-        SQL_OPERATION_MAPPING.put("NotNull", "is not null");
-        SQL_OPERATION_MAPPING.put("IsNull", "is null");
-        SQL_OPERATION_MAPPING.put("Null", "is null");
-        SQL_OPERATION_MAPPING.put("NotLike", "not like");
-        SQL_OPERATION_MAPPING.put("Like", "like");
-        SQL_OPERATION_MAPPING.put("StartingWith", "like");
-        SQL_OPERATION_MAPPING.put("EndingWith", "like");
-        SQL_OPERATION_MAPPING.put("IsNotLike", "not like");
-        SQL_OPERATION_MAPPING.put("Containing", "like");
-        SQL_OPERATION_MAPPING.put("NotContaining", "not like");
-        SQL_OPERATION_MAPPING.put("Not", "!=");
-        SQL_OPERATION_MAPPING.put("IsTrue", "is true");
-        SQL_OPERATION_MAPPING.put("True", "is true");
-        SQL_OPERATION_MAPPING.put("IsFalse", "is false");
-        SQL_OPERATION_MAPPING.put("False", "is false");
+        OPERATION_MAPPER.put("EQ", (column, value) -> Criteria.where(column).is(value));
+        OPERATION_MAPPER.put("Equal", (column, value) -> Criteria.where(column).is(value));
+        OPERATION_MAPPER.put("After", (column, value) -> Criteria.where(column).greaterThan(value));
+        OPERATION_MAPPER.put("GreaterThanEqual", (column, value) -> Criteria.where(column).greaterThanOrEquals(value));
+        OPERATION_MAPPER.put("GTE", (column, value) -> Criteria.where(column).greaterThanOrEquals(value));
+        OPERATION_MAPPER.put("GreaterThan", (column, value) -> Criteria.where(column).greaterThan(value));
+        OPERATION_MAPPER.put("GT", (column, value) -> Criteria.where(column).greaterThan(value));
+        OPERATION_MAPPER.put("Before", (column, value) -> Criteria.where(column).lessThan(value));
+        OPERATION_MAPPER.put("LessThanEqual", (column, value) -> Criteria.where(column).lessThanOrEquals(value));
+        OPERATION_MAPPER.put("LTE", (column, value) -> Criteria.where(column).lessThanOrEquals(value));
+        OPERATION_MAPPER.put("LessThan", (column, value) -> Criteria.where(column).lessThan(value));
+        OPERATION_MAPPER.put("LT", (column, value) -> Criteria.where(column).lessThan(value));
+
+        OPERATION_MAPPER.put("Between", (column, value) -> {
+            String[] range = value.toString().split(",");
+            return Criteria.where(column).between(range[0], range[1]);
+        });
+
+        OPERATION_MAPPER.put("NotBetween", (column, value) -> {
+            String[] range = value.toString().split(",");
+            return Criteria.where(column).notBetween(range[0], range[1]);
+        });
+
+        OPERATION_MAPPER.put("In", (column, value) -> {
+            List<Object> values = Arrays.stream(value.toString().split(","))
+                    .collect(Collectors.toList());
+            return Criteria.where(column).in(values);
+        });
+
+        OPERATION_MAPPER.put("NotIn", (column, value) -> {
+            List<Object> values = Arrays.stream(value.toString().split(","))
+                    .collect(Collectors.toList());
+            return Criteria.where(column).notIn(values);
+        });
+
+        OPERATION_MAPPER.put("IsNull", (column, value) -> Criteria.where(column).isNull());
+        OPERATION_MAPPER.put("NotNull", (column, value) -> Criteria.where(column).isNotNull());
+        OPERATION_MAPPER.put("IsNotNull", (column, value) -> Criteria.where(column).isNotNull());
+        OPERATION_MAPPER.put("Null", (column, value) -> Criteria.where(column).isNull());
+
+        OPERATION_MAPPER.put("Like", (column, value) -> Criteria.where(column).like(value.toString()));
+        OPERATION_MAPPER.put("NotLike", (column, value) -> Criteria.where(column).notLike(value.toString()));
+        OPERATION_MAPPER.put("StartingWith", (column, value) -> Criteria.where(column).like(value + "%"));
+        OPERATION_MAPPER.put("EndingWith", (column, value) -> Criteria.where(column).like("%" + value));
+        OPERATION_MAPPER.put("Containing", (column, value) -> Criteria.where(column).like("%" + value + "%"));
+        OPERATION_MAPPER.put("NotContaining", (column, value) -> Criteria.where(column).notLike("%" + value + "%"));
+
+        OPERATION_MAPPER.put("Not", (column, value) -> Criteria.where(column).not(value));
+        OPERATION_MAPPER.put("IsTrue", (column, value) -> Criteria.where(column).is(true));
+        OPERATION_MAPPER.put("True", (column, value) -> Criteria.where(column).is(true));
+        OPERATION_MAPPER.put("IsFalse", (column, value) -> Criteria.where(column).is(false));
+        OPERATION_MAPPER.put("False", (column, value) -> Criteria.where(column).is(false));
     }
 
     /**
@@ -183,22 +205,20 @@ public final class QueryJsonHelper {
      * @return A QueryFragment containing the constructed SQL conditions for querying JSON data.
      * @throws IllegalArgumentException If any processing error occurs due to invalid input structure or content.
      */
-    public static QueryFragment queryJson(Map<String, Object> params, String prefix) {
-        QueryFragment queryFragment = QueryFragment.withNew();
+    public static Condition queryJson(Map<String, Object> params, String prefix) {
+        Criteria criteria = Criteria.empty();
         for (Map.Entry<String, Object> entry : params.entrySet()) {
-            var condition = buildJsonCondition(entry, prefix);
-            queryFragment.getWhere().merge(condition.getWhere());
-            queryFragment.putAll(condition);
+            criteria = criteria.and(buildJsonCondition(entry));
         }
-        return queryFragment;
+        return Condition.of(criteria, prefix);
     }
 
     /**
      * Constructs a {@link QueryFragment} based on the provided map entry and a prefix.
      * The entry's key represents a JSON path, and the value is the condition's operand.
      * The method supports nested JSON paths and translates them into equivalent SQL expressions
-     * compatible with JSON columns in SQL databases. It validates the path, processes the keys,
-     * and delegates the construction of the final condition part to {@link #buildLastCondition(String[], Object)}.
+     * compatible with JSON column in SQL databases. It validates the path, processes the keys,
+     * and delegates the construction of the final condition part to .
      * <pre>
      * {@code
      * // Create a map of JSON-based from parameters
@@ -216,102 +236,43 @@ public final class QueryJsonHelper {
      * }
      * </pre>
      *
-     * @param entry  A map entry toSql the key is a dot-delimited string indicating a JSON path
-     *               (e.g., "extend.usernameLike"), and the value is the target value for the condition.
-     * @param prefix A prefix to prepend to the first key of the JSON path to qualify column names
-     *               in the SQL from, ensuring correct scoping or avoiding naming collisions.
+     * @param entry A map entry toSql the key is a dot-delimited string indicating a JSON path
+     *              (e.g., "extend.usernameLike"), and the value is the target value for the condition.
      * @return A {@link QueryFragment} object representing the constructed SQL condition
      * for querying JSON data, including the SQL fragment, parameters, and the operation detail.
      * @throws RestServerException If the provided JSON path does not meet the minimum requirement of specifying
      *                             at least one level of nesting after the prefix (if provided).
      */
-    private static QueryFragment buildJsonCondition(Map.Entry<String, Object> entry, String prefix) {
+    private static Criteria buildJsonCondition(Map.Entry<String, Object> entry) {
         String[] keys = StringUtils.delimitedListToStringArray(entry.getKey(), ".");
         if (keys.length < 2) {
-            throw QueryException.withError("Json from column path [from[" + entry.getKey() + "]] error",
+            throw QueryException.withError("Json from column path [query[" + entry.getKey() + "]] error",
                     new IllegalArgumentException("Json path example: extend.username," +
                             "Request from params:" +
-                            "from[extend.usernameLike]=aa," +
-                            "from[extend.age]=23," +
-                            "from[extend.nameIn]=aa,bb,cc," +
-                            "from[extend.codeEq]=123456"
+                            "query[extend.usernameLike]=aa," +
+                            "query[extend.age]=23," +
+                            "query[extend.nameIn]=aa,bb,cc," +
+                            "query[extend.codeEq]=123456"
                     ));
         }
         String column = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, keys[0]);
-        if (StringUtils.hasLength(prefix)) {
-            column = prefix + "." + keys[0];
-        }
-        StringBuilder conditionBuilder = new StringBuilder(column);
+        StringBuilder columnBuilder = new StringBuilder(column);
         int lastIndex = keys.length - 1;
         if (lastIndex > 1) {
             String[] joinKeys = Arrays.copyOfRange(keys, 1, lastIndex);
             for (String path : joinKeys) {
-                conditionBuilder.append("->'").append(escapeJsonKey(path)).append("'");
+                columnBuilder.append("->'").append(escapeJsonKey(path)).append("'");
             }
         }
-        QueryFragment lastCondition = buildLastCondition(keys, entry.getValue());
-        conditionBuilder.append(lastCondition.getWhere());
-        return QueryFragment.withParams(lastCondition).where(conditionBuilder.toString());
-    }
-
-    /**
-     * Constructs the final part of a from condition based on an array of keys and a value.
-     * This method generates SQL fragments for different types of conditions such as equality,
-     * between, in, not in, etc., depending on the detected keyword in the last key of the keys array.
-     * It handles parameterization to prevent SQL injection and prepares the condition for dynamic
-     * from execution.
-     *
-     * @param keys  An array of strings forming the path to the JSON attribute. The last element
-     *              may contain a keyword suffix for special operations.
-     * @param value The value to be compared against in the form condition. For 'In' and 'Between'
-     *              operations, this should be a comma-separated string or an array respectively.
-     * @return A QueryCondition object containing:
-     * - The 'getWhereSql' field as a string of the SQL fragment representing the condition.
-     * - The 'params' map holding named parameters and their values for the form.
-     * - The 'operation' entry describing the operation type and its SQL syntax.
-     */
-    private static QueryFragment buildLastCondition(String[] keys, Object value) {
-        StringBuilder conditionSql = new StringBuilder("->>'");
-
-        String paramName = StringUtils.arrayToDelimitedString(keys, "_");
-        String lastKey = keys[keys.length - 1];
-
-        Map.Entry<String, String> exps = queryKeywordMapper(lastKey);
-        if (exps == null) {
-            conditionSql.append(escapeJsonKey(lastKey)).append("' = :").append(paramName);
-            return QueryFragment.withParams(Map.of(paramName, value)).where(conditionSql.toString());
+        String lastKey = keys[lastIndex];
+        Map.Entry<String, BiFunction<String, Object, Criteria>> comparator = queryKeywordMapper(lastKey);
+        BiFunction<String, Object, Criteria> func = (col, value) -> Criteria.where(col).is(value);
+        if (comparator != null) {
+            lastKey = lastKey.substring(0, lastKey.length() - comparator.getKey().length());
+            func = comparator.getValue();
         }
-
-        String key = lastKey.substring(0, lastKey.length() - exps.getKey().length());
-        conditionSql.append(escapeJsonKey(key)).append("' ");
-        Map<String, Object> params;
-
-        value = switch (exps.getKey()) {
-            case "StartingWith" -> value + "%";
-            case "EndingWith" -> "%" + value;
-            case "Containing" -> "%" + value + "%";
-            default -> value;
-        };
-
-        if ("Between".equals(exps.getKey()) || "NotBetween".equals(exps.getKey())) {
-            String startKey = paramName + "_start";
-            String endKey = paramName + "_end";
-            conditionSql.append(exps.getValue()).append(" :").append(startKey).append(" and :").append(endKey);
-            var values = StringUtils.commaDelimitedListToStringArray(String.valueOf(value));
-            if (values.length < 2) {
-                throw QueryException.withError("Json between error",
-                        new IllegalArgumentException("Between operation requires two values"));
-            }
-            params = Map.of(startKey, values[0], endKey, values[1]);
-        } else if ("NotIn".equals(exps.getKey()) || "In".equals(exps.getKey())) {
-            conditionSql.append(exps.getValue()).append(" (:").append(paramName).append(")");
-            var values = StringUtils.commaDelimitedListToSet(String.valueOf(value));
-            params = Map.of(paramName, values);
-        } else {
-            conditionSql.append(exps.getValue()).append(" :").append(paramName);
-            params = Map.of(paramName, value);
-        }
-        return QueryFragment.withParams(params).where(conditionSql.toString());
+        columnBuilder.append("->>'").append(escapeJsonKey(lastKey)).append("'");
+        return func.apply(columnBuilder.toString(), entry.getValue());
     }
 
     /**
@@ -337,8 +298,8 @@ public final class QueryJsonHelper {
      * @return An entry containing the matched keyword and its corresponding value from the SQL_OPERATION_MAPPING,
      * or null if no match is found.
      */
-    private static Map.Entry<String, String> queryKeywordMapper(String queryKeyword) {
-        return SQL_OPERATION_MAPPING.entrySet().stream()
+    private static Map.Entry<String, BiFunction<String, Object, Criteria>> queryKeywordMapper(String queryKeyword) {
+        return OPERATION_MAPPER.entrySet().stream()
                 .filter(entry -> StringUtils.endsWithIgnoreCase(queryKeyword, entry.getKey()))
                 .max((entry1, entry2) -> {
                     int entry1Length = entry1.getKey().length();
@@ -346,6 +307,5 @@ public final class QueryJsonHelper {
                     return Integer.compare(entry1Length, entry2Length);
                 }).orElse(null);
     }
-
 
 }
