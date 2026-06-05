2cf5a4cf6ed313f3bd55a3987d76ebd3fe63d53d
AlexBob <billbob.dong@gmail.com>
2026-06-05 14:04:31 +0800

    feat(ng-plate): add i18n support and reconfigure build

--- ui/ng-plate/src/app/plugins/index.ts
+++ ui/ng-plate/src/app/plugins/index.ts
@@ -2,3 +2,4 @@ export { Modals, ModalsService, type ModalRef, type ModalOptions } from './modal
 export { MessageService } from './toasts';
 export { TransferComponent, type TransferItem, type TransferData } from './transfer';
 export { DataTableComponent, type DataTableColumn, type DataTableRow } from './data-table';
+export { Dayjs } from './dayjs.locale';
