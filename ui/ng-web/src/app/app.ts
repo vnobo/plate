c2eb0acf767030d2af092e076eb76bf9c386af1c
AlexBob <billbob.dong@gmail.com>
2026-06-05 12:44:13 +0800

    feat: add i18n and trellis skill, replace template with test page

--- ui/ng-web/src/app/app.ts
+++ ui/ng-web/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { DatePipe, DecimalPipe, CurrencyPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import dayjs from './core/dayjs.locale';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe, DecimalPipe, CurrencyPipe],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('ng-web');
+
+  // i18n test data
+  protected readonly currentDate = new Date();
+  protected readonly dayjsDate = dayjs();
+  protected readonly sampleNumber = 1234567.89;
+  protected readonly samplePrice = 99.99;
 }
