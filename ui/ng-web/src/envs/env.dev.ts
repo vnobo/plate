3b26dd881e8f97ffe9be58aa1159418fdd944990
AlexBob <billbob.dong@gmail.com>
2026-06-08 19:31:03 +0800

    chore(boot): update gitignore for local config

--- ui/ng-web/src/envs/env.dev.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  host: '/api',
-  relaApiPath: '/rela',
-  secApiPath: '/sec',
-};
--- ui/ng-web/src/envs/env.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  host: '',
-  relaApiPath: '/rel',
-  secApiPath: '/sec',
-};
